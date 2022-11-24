089895aa0f72e24386ca4959c48c859ee4d91390
Joao Teles <joaojoseteles@gmail.com>
2022-11-24 16:56:17 -0300

    :bulb: docs: updating project

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
 import Button from "../src/Components/Button/Button";
+import ThemeProvider from "../src/theme/ThemeProvider";
 
 export default function HomeScreen() {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <Button>clique aqui</Button>
-    </div>
+    <ThemeProvider>
+      <div>
+        <h1>Home Page</h1>
+        <Button variant="primary">clique aqui</Button>
+      </div>
+    </ThemeProvider>
   );
 }
--- src/Components/Button/Button.tsx
+++ src/Components/Button/Button.tsx
@@ -1,11 +1,31 @@
+import styled from "styled-components";
+
+const StyledButton = styled.button<ButtonProps>`
+  cursos: poiter;
+  border: 0;
+  padding: 8px 14px;
+  transition: 0.3s;
+  ${({ theme, variant }) => {
+    console.log(theme);
+    return {
+      backgroundColor: theme.colors[variant].main,
+      color: theme.colors[variant].text,
+      ":hover": {
+        backgroundColor: theme.colors[variant].light,
+      },
+    };
+  }}
+`;
 interface ButtonProps {
   disabled?: boolean;
   children: React.ReactNode;
+  variant?: "primary" | "accent";
 }
 
 export default function Button({ children, ...props }: ButtonProps) {
-  return <button {...props}>{children}</button>;
+  return <StyledButton {...props}>{children}</StyledButton>;
 }
 Button.defaultProps = {
   disabled: false,
+  variant: "primary",
 };
